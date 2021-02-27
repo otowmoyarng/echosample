67ea6ff0fc12734ce125df70dbf7c49a00bd0145
hidemusiam <hidemusiam@gmail.com>
2021-02-27 10:43:49 +0900

    GETリクエスト実装完了

--- server.go
+++ server.go
@@ -6,10 +6,34 @@ import (
 	"github.com/labstack/echo"
 )
 
+type Project struct {
+	Team   string `json:"team"`
+	Menber string `json:"menber"`
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	e.GET("/index", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello echo")
 	})
+	e.GET("/index/:id", pathget)
+	e.GET("/project", queryget)
+	e.POST("/project", queryget)
 	e.Logger.Fatal(e.Start(":1234"))
+}
+
+func pathget(c echo.Context) error {
+	return c.String(http.StatusOK, c.Param("id"))
+}
+
+func queryget(c echo.Context) error {
+
+	result := new(Project)
+
+	if err := c.Bind(result); err != nil {
+		return err
+	}
+	result.Team += "hoge"
+	result.Menber += "yamada"
+	return c.JSON(http.StatusOK, result)
 }
